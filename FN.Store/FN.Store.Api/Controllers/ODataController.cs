ee9dfcbfda4ba91e0f5be252b50f2c99553b9c7f
Rodrigo <rodrigo.cocate@hotmail.com>
2017-07-18 16:04:47 -0700

    Subindo módulo 5

---
+++ FN.Store/FN.Store.Api/Controllers/ODataController.cs
@@ -0,0 +1,24 @@
+﻿using FN.Store.Data.EF.Repository;
+using FN.Store.Domain.Entities;
+using FN.Store.Domain.Repositories;
+using System.Linq;
+using System.Web.Http;
+
+namespace FN.Store.Api.Controllers
+{
+    public class ODataController : ApiController
+    {
+        private readonly IClienteRepository _clienteRepo = new ClienteRepository();
+
+        [Queryable]
+        public IQueryable<Cliente> Get()
+        {
+            return _clienteRepo.ObterOData();
+        }
+        
+        protected override void Dispose(bool disposing)
+        {
+            _clienteRepo.Dispose();
+        }
+    }
+}
---
+++ FN.Store/FN.Store.Api/Infra/AuthenticationMessageHandler.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Dispatcher;
+
+namespace FN.Store.Api.Infra
+{
+    public class AuthenticationMessageHandler : DelegatingHandler
+    {
+        public AuthenticationMessageHandler(HttpConfiguration httpConfig)
+        {
+            InnerHandler = new HttpControllerDispatcher(httpConfig);
+
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            HttpResponseMessage response;
+
+            if (request.Headers.Authorization != null &&
+                request.Headers.Authorization.Scheme == "Basic")
+            {
+                var encode = request.Headers.Authorization.Parameter.Trim();
+                var user = Encoding.Default.GetString(Convert.FromBase64String(encode));
+                var data = user.Split(":".ToCharArray());
+                var username = data[0];
+                var password = data[1];
+
+                if (!(username == "admin" && password == "123456"))
+                {
+                    response = request.CreateResponse(HttpStatusCode.Unauthorized);
+                    return response;
+                }
+            }
+            else {
+                response = request.CreateResponse(System.Net.HttpStatusCode.Unauthorized);
+                return response;
+            }
+            response = await base.SendAsync(request, cancellationToken);
+
+            if (response.StatusCode == HttpStatusCode.Unauthorized)
+            {
+                response.Headers.Add("WWW-Authenticate", "Basic");
+            }
+
+            return response;
+        }
+    }
+}
