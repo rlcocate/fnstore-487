90ba243732810740286ea150fc9e5b80f4fd04c6
Rodrigo <rodrigo.cocate@hotmail.com>
2017-07-18 17:36:50 -0700

    WCF com Rest

---
+++ FN.Store/FN.Store.WCFData/Model/ClienteVM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace FN.Store.WCFData.Model
+{
+    [DataContract]
+    public class ClienteVM
+    {
+        public ClienteVM()
+        {
+            DataLog = DateTime.Now;
+        }
+
+        [DataMember]
+        public int Id { get; set; }
+        
+        [DataMember]
+        public string Nome { get; set; }
+        
+        [DataMember]
+        public DateTime DataLog { get; set; }
+    }
+}
---
+++ FN.Store/FN.Store.WCFData/Service/IClienteService.cs
@@ -0,0 +1,20 @@
+﻿using FN.Store.WCFData.Model;
+using System.Collections.Generic;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace FN.Store.WCFData.Service
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IClienteService" in both code and config file together.
+    [ServiceContract]
+    public interface IClienteService
+    {
+        [OperationContract]
+        [WebGet(UriTemplate="clientes", ResponseFormat=WebMessageFormat.Json)]
+        IEnumerable<ClienteVM> ObterTodos();
+
+        [OperationContract]
+        [WebGet(UriTemplate="clientes/{id}", ResponseFormat=WebMessageFormat.Json)]
+        ClienteVM ObterPorId(string id);
+    }
+}
